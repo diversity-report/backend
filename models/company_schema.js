b37c42be9a11711b7558c747982a7e6f4d250186
Aliza Pilisuk <apilisuk@gmail.com>
2016-11-05 14:14:30 -0700

    changed schemas for HR reporting and removed unnecessary routes

--- models/company_schema.js
+++ models/company_schema.js
@@ -15,7 +15,67 @@ const companySchema = new mongoose.Schema({
   state: {
     type: String,
     required: true
-  }
+  },
+  gender: {
+    female: {
+      type: Number,
+      default: 0
+    },
+    male: {
+      type: Number,
+      default: 0
+    },
+    nonbinary: {
+      type: Number,
+      default: 0
+    },
+    unreported: {
+      type: Number,
+      default: 0
+    }
+  },
+  race: {
+    arabMiddleEastern: {
+      type: Number,
+      default: 0
+    },
+    latinoHispanic: {
+      type: Number,
+      default: 0
+    },
+    blackAfricanAmerican: {
+      type: Number,
+      default: 0
+    },
+    asianPacificIslander: {
+      type: Number,
+      default: 0
+    },
+    nativeAmericanAlaskanNative: {
+      type: Number,
+      default: 0
+    },
+    nativeHawaiianPacificIslander: {
+      type: Number,
+      default: 0
+    },
+    whiteCaucasian: {
+      type: Number,
+      default: 0
+    },
+    unreported: {
+      type: Number,
+      default: 0
+    }
+  },
+  veteran: {
+    type: Number,
+    default: 0
+  },
+  dependants: {
+    type: Number,
+    default: 0
+  },
 });
 
 module.exports = mongoose.model('Company', companySchema);
