68502109ebf12f7ea11ad805642c0f0de4aceaf8
Sawako <sawako.yoriko@gmail.com>
2016-11-05 23:51:53 -0700

    fixed get /newCompany route and schema

--- models/company_schema.js
+++ models/company_schema.js
@@ -2,84 +2,29 @@
 
 const mongoose = require('mongoose');
 
-const companySchema = new mongoose.Schema({
-  companyName: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  city: {
-    type: String,
-    required: true
-  },
-  state: {
-    type: String,
-    required: true
-  },
-  numOfEmployees: {
-    type: Number,
-    required: true
-  },
+const companySchema = mongoose.Schema({
+  companyName: { type: String, required: true, unique: true },
+  city: { type: String, required: true },
+  state: { type: String,required: true},
+  numOfEmployees: { type: Number, required: true },
   gender: {
-    female: {
-      type: Number,
-      default: 0
-    },
-    male: {
-      type: Number,
-      default: 0
-    },
-    nonbinary: {
-      type: Number,
-      default: 0
-    },
-    unreported: {
-      type: Number,
-      default: 0
-    }
+    female: { type: Number },
+    male: { type: Number },
+    nonbinary: { type: Number },
+    unreported: { type: Number }
   },
-  race: {
-    arabMiddleEastern: {
-      type: Number,
-      default: 0
-    },
-    latinoHispanic: {
-      type: Number,
-      default: 0
-    },
-    blackAfricanAmerican: {
-      type: Number,
-      default: 0
-    },
-    asianPacificIslander: {
-      type: Number,
-      default: 0
-    },
-    nativeAmericanAlaskanNative: {
-      type: Number,
-      default: 0
-    },
-    nativeHawaiianPacificIslander: {
-      type: Number,
-      default: 0
-    },
-    whiteCaucasian: {
-      type: Number,
-      default: 0
-    },
-    unreported: {
-      type: Number,
-      default: 0
-    }
+  race: { 
+    arabMiddleEastern: { type: Number },
+    latinoHispanic: { type: Number },
+    blackAfricanAmerican: { type: Number },
+    asianPacificIslander: { type: Number },
+    nativeAmericanAlaskanNative: { type: Number },
+    nativeHawaiianPacificIslander: { type: Number },
+    whiteCaucasian: { type: Number },
+    unreported: { type: Number }
   },
-  veteran: {
-    type: Number,
-    default: 0
-  },
-  dependants: {
-    type: Number,
-    default: 0
-  },
+  veteran: { type: Number },
+  dependants: { type: Number }
 });
 
 module.exports = mongoose.model('Company', companySchema);
